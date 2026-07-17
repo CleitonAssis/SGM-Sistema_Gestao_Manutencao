5d1040c4bd8d5e4350cf8b8528ad0fc793d89f41
Cleiton Vinicius de Assis <87096339+CleitonAssis@users.noreply.github.com>
2026-07-17 16:04:14 -0300

    Add files via upload

---
+++ errors/400.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+  <head>
+    <!-- Meta tags Obrigatórias -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!--<meta http-equiv="refresh" content="5">--><!--Recarrega a página automaticamente de 5 em 5 segundos-->
+    <link rel="shortcut icon" href="/sgm/errors/img/favicon.ico" />
+    <link rel="stylesheet" href="/sgm/errors/css/errors.css">
+
+    <title> ERRO 400</title>
+  </head>
+  <body>
+    <div class="container">
+      <div class="centralizar">
+        <h1 class="errorConexao"><img class="icone" src="/sgm/errors/img/warning-icon.svg" alt="Ícone de alerta"> Requisição inválida !</h1>
+        <img style="width: 20rem;" src="/sgm/errors/img/erro400.svg">
+      </div>
+    </div>
+
+
+    <!--Função redireciona para página anterior automáticamente após 20 segundos -->
+    <script type="text/javascript">
+    function redireciona() {
+      setTimeout(() => {
+        location.href = '/sgm';
+      }, 20000);
+    }
+    window.addEventListener('load', redireciona);
+    </script>
+    
+  </body>
+</html>
